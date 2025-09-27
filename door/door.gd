6b353e5b8ef6fdba66062c167e855e47414520b3
Armand <174164929+NanoMars@users.noreply.github.com>
2025-09-27 22:24:23 +1000

    finish up door

---
+++ door/door.gd
@@ -0,0 +1,20 @@
+extends Node2D
+@onready var area_2d: Area2D = $Area2D
+@onready var count_label: Label = $Sprite2D2/SubViewport/Label
+@export var required_players: int = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	count_label.text = str(required_players)
+	area_2d.body_entered.connect(_on_body_entered)
+	$DoorEnteredSound.max_polyphony = required_players
+
+func _on_body_entered(body: Node) -> void:
+	if body is CharacterBody2D and body.is_in_group("player"):
+		required_players = max(required_players - 1, 0)
+		count_label.text = str(required_players)
+		body.queue_free()
+		$DoorEnteredSound.play()
+		if required_players <= 0:
+			$DoorUnlockedSound.play()
+			
