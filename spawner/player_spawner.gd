d09a013ea8d6e89bd154177aecd82fd08b57274a
Armand <174164929+NanoMars@users.noreply.github.com>
2025-09-28 14:09:29 +1000

    spawner has an option to spawn infinitely

--- spawner/player_spawner.gd
+++ spawner/player_spawner.gd
@@ -3,9 +3,21 @@ extends Marker2D
 @export var spawn_count: int = 25
 @export var player_scene: PackedScene
 
+@export var spawn_infinitely: bool = false
+
 var players: Array[CharacterBody2D] = []
 
+var timer: Timer
+
 func _ready() -> void:
+	if spawn_infinitely:
+		timer = Timer.new()
+		timer.wait_time = spawn_timer
+		timer.one_shot = false
+		timer.autostart = true
+		add_child(timer)
+		timer.timeout.connect(_on_timer_timeout)
+		return
 	GameManager.allow_restart = false
 	await get_tree().create_timer(0.5).timeout
 	for i in range(spawn_count):
@@ -23,4 +35,8 @@ func _ready() -> void:
 		p.enabled = true
 	GameManager.allow_restart = true
 
-
+func _on_timer_timeout() -> void:
+	var player_instance = player_scene.instantiate()
+	player_instance.global_position = global_position
+	get_tree().get_first_node_in_group("game_root").add_child(player_instance)
+	players.append(player_instance)
