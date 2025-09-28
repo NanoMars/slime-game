7fab25f43573c85448079aceb37861ac87fcd5cc
Armand <174164929+NanoMars@users.noreply.github.com>
2025-09-28 16:15:17 +1000

    push button

--- UI/title_screen.gd
+++ UI/title_screen.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	anim_player.play("anim")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
