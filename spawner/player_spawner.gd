a2d37d61183045c73666146d3c77cbf03c18fd40
Armand <174164929+NanoMars@users.noreply.github.com>
2025-09-28 13:40:54 +1000

    commit stuff

--- spawner/player_spawner.gd
+++ spawner/player_spawner.gd
@@ -2,21 +2,25 @@ extends Marker2D
 @export var spawn_timer: float = 2.0
 @export var spawn_count: int = 25
 @export var player_scene: PackedScene
-@onready var game_manager: Node = get_tree().get_root().get_node("GameManager")
 
 var players: Array[CharacterBody2D] = []
 
 func _ready() -> void:
 	GameManager.allow_restart = false
-	for i in spawn_count:
+	await get_tree().create_timer(0.5).timeout
+	for i in range(spawn_count):
+		print("Spawning player ", i)
 		var player_instance = player_scene.instantiate()
 		player_instance.global_position = global_position
-		await get_tree().get_first_node_in_group("game_root").call_deferred("add_child", player_instance)
+		# Schedule add_child next frame; no await here
+		get_tree().get_first_node_in_group("game_root").add_child.call_deferred(player_instance)
 		players.append(player_instance)
-		await get_tree().create_timer(spawn_timer / spawn_count).timeout
+		# Only wait between spawns
+		if i < spawn_count - 1:
+			await get_tree().create_timer(spawn_timer / max(1, spawn_count)).timeout
 
-	for i in players:
-		i.enabled = true
+	for p in players:
+		p.enabled = true
 	GameManager.allow_restart = true
-	
-	
+
+
